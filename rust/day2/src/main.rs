a3aa69ee2efbce437f6fa52670539bdf55ee9138
David Barthelemy <davidbarthelemy28@gmail.com>
2021-12-02 22:56:38 +0100

    day2 in rust

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -2,13 +2,46 @@ use day2::util;
 
 fn main() {
     let lines = util::read_lines("input.txt").unwrap();
-    //let lines_int = util::read_ints("input.txt");
     part1(&lines);
     part2(&lines);
 }
 
 fn part1(lines: &Vec<String>) {
+    let mut position = 0;
+    let mut depth = 0;
+    for l in lines {
+        if let Some((instruction, value)) = l.split_once(" ") {
+            let int_value = value.parse::<i32>().unwrap();
+            match instruction {
+                "forward" => position += int_value,
+                "down" => depth += int_value,
+                "up" => depth -= int_value,
+                _ => {}
+            }
+        }
+
+    }
+    println!("{}", position * depth);
 }
 
 fn part2(lines: &Vec<String>) {
+    let mut position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for l in lines {
+        if let Some((instruction, value)) = l.split_once(" ") {
+            let i_value = value.parse::<i32>().unwrap();
+            match instruction {
+                "forward" => {
+                    position += i_value;
+                    depth += aim * i_value
+                },
+                "down" => aim += i_value,
+                "up" => aim -= i_value,
+                _ => {}
+            }
+        }
+
+    }
+    println!("{}", position * depth);
 }
